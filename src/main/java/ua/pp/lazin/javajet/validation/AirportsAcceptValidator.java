18bcc348194f532c80c5c3590e89bb1b609ab0a3
ruslanlazin <ruslanlazin@gmail.com>
2017-01-17 13:18:28 +0200

    add messages for validation

--- src/main/java/ua/pp/lazin/javajet/validation/AirportsAcceptValidator.java
+++ src/main/java/ua/pp/lazin/javajet/validation/AirportsAcceptValidator.java
@@ -12,20 +12,21 @@ import java.util.Properties;
  * @author Ruslan Lazin
  */
 public class AirportsAcceptValidator extends AbstractValidator<Flight> {
-    private static final Properties prohibitedAirportCodes = PropertiesLoader.loadPropertiesFromFile("prohibitedAirports.properties");
+    private static final Properties prohibitedAirportCodes =
+            PropertiesLoader.loadPropertiesFromFile("prohibitedAirports.properties");
 
     @Override
     public Errors validate(Flight flight, Errors errors) {
 
         if (prohibitedAirportCodes.containsKey(flight.getDeparture().getIataCode())) {
-            errors.addError(new ObjectError("Flying to Departure Airport " +
-                    flight.getDeparture().getName() + "prohibited by " +
+            errors.addError(new ObjectError("Flying to Airport " +
+                    flight.getDeparture().getName() + " was prohibited by " +
                     prohibitedAirportCodes.getProperty(flight.getDeparture().getIataCode()),
                     "errors.airport.not.acceptable"));
         }
         if (prohibitedAirportCodes.containsKey(flight.getDestination().getIataCode())) {
-            errors.addError(new ObjectError("Flying to Destination Airport " +
-                    flight.getDestination().getName() + "prohibited by " +
+            errors.addError(new ObjectError("Flying to Airport " +
+                    flight.getDestination().getName() + " was prohibited by " +
                     prohibitedAirportCodes.getProperty(flight.getDestination().getIataCode()),
                     "errors.airport.not.acceptable"));
         }
